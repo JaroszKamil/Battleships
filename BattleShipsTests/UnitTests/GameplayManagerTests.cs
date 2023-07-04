a43cc69c786f28f91e2dc1203ed6e9bf31d2fcff
Kamil Jarosz <kamiljarosz@outlook.com>
2023-07-05 00:05:29 +0200

    add some tests

--- BattleShipsTests/UnitTests/GameplayManagerTests.cs
+++ BattleShipsTests/UnitTests/GameplayManagerTests.cs
@@ -19,7 +19,7 @@ namespace BattleShipsApi.Tests.UnitTests
         }
 
         [Fact]
-        public void PlayerShoot_ShipCoordinates_ShouldReturnShipWithOneHit()
+        public void PlayerShoot_OnlyOneShipCoordinate_ShouldReturnShipWithOneHit()
         {
             //Arrange
             var shipCoordinates = board.TargetGrid.OfType<GridCell<Ship>>().GroupBy(x => x.CellContent.ShipGuid).First();
@@ -28,11 +28,39 @@ namespace BattleShipsApi.Tests.UnitTests
 
             //Act
             var result = this.gameplayManager.PlayerShoot(shipCell);
-            
+
+            var ship = result.As<GridCell<Ship>>().CellContent.As<Ship>();
+
             //Assert
             result.Should().BeOfType<GridCell<Ship>>();
             result.OcenCellStatus.Should().Be(CellStatusEnum.hits);
-            result.As<GridCell<Ship>>().CellContent.As<Ship>().SizeOnGrid.Should().Be(aspectedShipOnGridSize);
+            ship.SizeOnGrid.Should().Be(aspectedShipOnGridSize);
+            ship.IsAlive.Should().BeTrue();
+        }
+
+        [Fact]
+        public void PlayerShoot_AllShipCoordinates_ShipShouldBeSink()
+        {
+            //Arrange
+            var shipCoordinates = board.TargetGrid.OfType<GridCell<Ship>>().GroupBy(x => x.CellContent.ShipGuid).First();
+            var shipCell = shipCoordinates.First();
+            var aspectedShipOnGridSize = shipCell.As<GridCell<Ship>>().CellContent.As<Ship>().SizeOnGrid - 1;
+
+            //Act
+            GridCoordinates? result = null;
+
+            shipCoordinates.ToList().ForEach(x =>
+            {
+                result = this.gameplayManager.PlayerShoot(shipCell);
+            });
+
+            var ship = result.As<GridCell<Ship>>().CellContent.As<Ship>();
+
+            //Assert
+            result.Should().BeOfType<GridCell<Ship>>();
+            result?.OcenCellStatus.Should().Be(CellStatusEnum.sinks);
+            ship.IsAlive.Should().BeFalse();
+            ship.SizeOnGrid.Should().Be(0);
         }
     }
 }
